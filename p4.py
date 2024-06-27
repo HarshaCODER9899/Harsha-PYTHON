d8173275b63d989f28c5481f55917b8a49ae4007
HarshaCODER9899 <harshasai569@gmail.com>
2024-06-27 19:04:50 +0530

    Update p4.py

--- p4.py
+++ p4.py
@@ -1,18 +1,19 @@
-#the time complexity of my_max is O(n)
+#the time complexity of checking_vowel is O(n)
 
 # the memory usage is 160 bytes
 
 
 
-def vowel(v):
-    lst=["a","e","i","o","u","A","E","I","O","U"]
+def checking_vowel(v):
+    #gives true for an vowel and false for a consolent
+    vowels=["a","e","i","o","u","A","E","I","O","U"]
     l1=[]
     for x in v:
-        if x in lst:
+        if x in vowels:
             l1.append("True")
         else:    
          l1.append("False")
     return l1    
 
 
-print(vowel("asdef"))      
+print(checking_vowel("asdef"))      
